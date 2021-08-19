da010eb10207c20a8102f38065851d0d5425ad57
Animody <86797313+Animody@users.noreply.github.com>
2021-08-20 06:59:34 +0800

    Add files via upload

---
+++ Rewrite/JS/YouTube_ad_Killer.js
@@ -0,0 +1,21 @@
+#YouTube_Ad_Killer
+
+****************************
+Solution 1:
+ctier=[A-Z] url 302 ctier=A
+^https://[\s\S]*\.googlevideo\.com/.*&(oad|ctier) url reject-200
+****************************
+Solution 2:
+http-request,pattern=^https?:\/\/.+?\.googlevideo\.com\/.+&(oad|ctier)=(?!A) script-request-header https://choler.github.io/Surge/Script/YouTube.js
+****************************
+
+
+
+if ($request.url.indexOf("&oad") != -1) {
+  $done({ response: {body: ""} });
+} else if ($request.url.indexOf("&ctier") != -1) {
+  let url = $request.url.replace(/ctier=[A-Z]/, "ctier=A");
+  $done({ response: { status: 302, headers: { Location: url } } });
+} else {
+  $done({})
+}
