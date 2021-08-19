55690ed7bee960c8c416febcd2f5b5b4680793ad
Animody <86797313+Animody@users.noreply.github.com>
2021-08-20 07:07:17 +0800

    Update YouTube_ad_Killer.js

--- Rewrite/JS/YouTube_ad_Killer.js
+++ Rewrite/JS/YouTube_ad_Killer.js
@@ -1,13 +1,11 @@
-/****************************
-     YouTube_Ad_Killer
-****************************
+/************YouTube_Ad_Killer****************
 Solution 1:
 ctier=[A-Z] url 302 ctier=A
 ^https://[\s\S]*\.googlevideo\.com/.*&(oad|ctier) url reject-200
-****************************
+**********************************************
 Solution 2:
 http-request,pattern=^https?:\/\/.+?\.googlevideo\.com\/.+&(oad|ctier)=(?!A) script-request-header https://choler.github.io/Surge/Script/YouTube.js
-****************************/
+**********************************************/
 
 if ($request.url.indexOf("&oad") != -1) {
   $done({ response: {body: ""} });
